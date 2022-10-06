2e2744311c8f1c652a01bf34601d4293bbe9e6d5
sergiolara92 <sergioldm92@gmail.com>
2022-10-06 22:05:14 +0200

    requisitos ok

--- javascript/contact.js
+++ javascript/contact.js
@@ -19,3 +19,15 @@ const handleClickSubmit= (event) => {
 }
 
 formulario.addEventListener("submit", handleClickSubmit);
+
+
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu');
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items');
+            menu_items.classList.toggle('show');
+        })
+        
+    }
+});
