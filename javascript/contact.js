0ac5de7ea30f42fbc8c0820971eb744a6ba295e9
sergiolara92 <sergioldm92@gmail.com>
2022-10-07 00:28:55 +0200

    dark mode ok

--- javascript/contact.js
+++ javascript/contact.js
@@ -31,3 +31,10 @@ addEventListener('DOMContentLoaded', () => {
         
     }
 });
+
+const switchButton = document.getElementById('switch');
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+});
--- javascript/project.js
+++ javascript/project.js
@@ -34,4 +34,11 @@ addEventListener('DOMContentLoaded', () => {
         
     }
 });
+
+const switchButton = document.getElementById('switch');
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+});
     
